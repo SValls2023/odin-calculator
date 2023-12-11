8dcdb521701a29605f8f7fe1f2ad380f420a83fe
SValls2023 <54817897+SValls2023@users.noreply.github.com>
2023-12-10 22:19:03 -0800

    "Feat: Implement CSS Grid and other styling features to improve UX"

---
+++ script.js
@@ -0,0 +1,9 @@
+/* Query Selectors */
+
+const prevOperand = document.querySelector('[data-previous-op]');
+const currOperand = document.querySelector('[curr-op]');
+const numBtn = document.querySelectorAll('[data-number]');
+const operationBtn = document.querySelectorAll('[data-operation]');
+const deleteBtn = document.querySelector('[data-delete]');
+const clearBtn = document.querySelector('[data-clear]');
+const computeBtn = document.querySelector('[data-compute');
